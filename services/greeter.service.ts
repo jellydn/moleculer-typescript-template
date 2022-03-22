25bddb960374ef54a6299ca043b235f5e6d9cc40
Huynh Duc Dung <dunghd.it@gmail.com>
2022-03-22 14:12:59 +0800

    feat: init typescript template for moleculer

---
+++ services/greeter.service.ts
@@ -0,0 +1,82 @@
+import { Context, ServiceSchema } from "moleculer";
+
+const greeterService: ServiceSchema = {
+	name: "greeter",
+
+	/**
+	 * Settings
+	 */
+	settings: {},
+
+	/**
+	 * Dependencies
+	 */
+	dependencies: [],
+
+	/**
+	 * Actions
+	 */
+	actions: {
+		/**
+		 * Say a 'Hello' action.
+		 *
+		 * @returns
+		 */
+		hello: {
+			rest: {
+				method: "GET",
+				path: "/hello",
+			},
+			async handler() {
+				return "Hello Moleculer";
+			},
+		},
+
+		/**
+		 * Welcome, a username
+		 *
+		 * @param {String} name - User name
+		 */
+		welcome: {
+			rest: "/welcome",
+			params: {
+				name: "string",
+			},
+			/** @param {Context} ctx  */
+			async handler(
+				ctx: Context<{
+					name: string;
+				}>
+			) {
+				return `Welcome, ${ctx.params.name}`;
+			},
+		},
+	},
+
+	/**
+	 * Events
+	 */
+	events: {},
+
+	/**
+	 * Methods
+	 */
+	methods: {},
+
+	/**
+	 * Service created lifecycle event handler
+	 */
+	// created() {},
+
+	/**
+	 * Service started lifecycle event handler
+	 */
+	// async started() {},
+
+	/**
+	 * Service stopped lifecycle event handler
+	 */
+	// async stopped() {},
+};
+
+export default greeterService;
---
+++ test/unit/services/greeter.spec.ts
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { Errors, ServiceBroker } from "moleculer";
+
+import TestGreeterService from "../../../services/greeter.service";
+
+const { ValidationError } = Errors;
+
+describe("Test 'greeter' service", () => {
+	const broker = new ServiceBroker({ logger: false });
+	broker.createService(TestGreeterService);
+
+	beforeAll(async () => broker.start());
+	afterAll(async () => broker.stop());
+
+	describe("Test 'greeter.hello' action", () => {
+		it("should return with 'Hello Moleculer'", async () => {
+			const response = await broker.call("greeter.hello");
+			expect(response).toBe("Hello Moleculer");
+		});
+	});
+
+	describe("Test 'greeter.welcome' action", () => {
+		it("should return with 'Welcome'", async () => {
+			const response = await broker.call("greeter.welcome", {
+				name: "Adam",
+			});
+			expect(response).toBe("Welcome, Adam");
+		});
+
+		it("should reject an ValidationError", async () => {
+			expect.assertions(1);
+			try {
+				await broker.call("greeter.welcome");
+			} catch (error: unknown) {
+				expect(error).toBeInstanceOf(ValidationError);
+			}
+		});
+	});
+});
