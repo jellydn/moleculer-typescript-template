48a366bbeebdb3c79ee71d943c81565711e047f8
Huynh Duc Dung <dunghd.it@gmail.com>
2022-05-10 03:09:26 +0800

    :sparkles: add socket io example

---
+++ examples/README.md
@@ -0,0 +1,20 @@
+# Examples
+
+## Apollo Server (GraphQL)
+
+```sh
+npx ts-node apollo-server
+```
+
+## Express
+
+```sh
+npx ts-node express
+```
+
+## Socket.io
+
+```sh
+npx ts-node socket
+serve socket/public -p 3001
+```
---
+++ examples/socket/index.ts
@@ -0,0 +1,42 @@
+import { ServiceBroker, ServiceSchema } from "moleculer";
+import SocketIOService from "moleculer-io";
+import ApiService from "moleculer-web";
+
+import config from "../../moleculer.config";
+
+// Create broker
+const broker = new ServiceBroker(config);
+broker.createService({
+	name: "gateway",
+	mixins: [ApiService, SocketIOService as ServiceSchema], // Should after moleculer-web
+	settings: {
+		// Exposed port
+		port: Number(process.env?.PORT ?? 3000),
+
+		// Exposed IP
+		ip: "0.0.0.0",
+
+		cors: {
+			origin: "*",
+			allowedHeaders: [],
+			exposedHeaders: [],
+			credentials: false,
+			maxAge: 3600,
+		},
+	},
+});
+
+// Math service
+broker.createService({
+	name: "math",
+	actions: {
+		add(ctx) {
+			return Number(ctx.params.a) + Number(ctx.params.b);
+		},
+	},
+});
+
+broker
+	.start()
+	.then(() => broker.repl())
+	.catch(console.error);
