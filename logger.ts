2898dd3a71367e20649f62b5e6a488e5381247af
Huynh Duc Dung <dunghd.it@gmail.com>
2022-11-06 22:51:33 +0800

    :heavy_minus_sign: remove pino-pretty

--- logger.ts
+++ logger.ts
@@ -1,15 +1,5 @@
 import pino from "pino";
 
-const isProduction = process.env.NODE_ENV === "production";
-
-export const logger = pino(
-	isProduction
-		? {
-				level: process.env.LEVEL ?? "warn", // Default is warn on production
-		  }
-		: {
-				transport: {
-					target: "pino-pretty",
-				},
-		  }
-);
+export const logger = pino({
+	level: process.env.LOGLEVEL ?? "info",
+});
