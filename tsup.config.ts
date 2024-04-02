ba48597436c718d0a1dccc17fe146c26b4535523
Huynh Duc Dung <dunghd.it@gmail.com>
2024-04-02 23:36:59 +0800

    chore: remove inline tsup
    chore: setup build

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-    entry: ["moleculer.config.ts", "services/**/*.ts", "addons/**/*.ts"],
+    entry: ["addons/**/*.ts", "logger.ts", "moleculer.config.ts", "services/**/*.ts"],
     splitting: false,
     sourcemap: true,
     format: ["esm", "cjs"],
