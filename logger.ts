d28f17b90d4ea0415fe8c505bd4b4823b3e5cc10
Huynh Duc Dung <dunghd.it@gmail.com>
2024-04-12 11:15:48 +0800

    feat(logger): enhance logger with standard time functions

--- logger.ts
+++ logger.ts
@@ -1,5 +1,7 @@
-import pino from "pino";
+import pino, { stdTimeFunctions } from "pino";
 
+// Usage: logger[logLevel]([mergingObject],[message])
 export const logger = pino({
     level: process.env.LOGLEVEL ?? "info",
+    timestamp: stdTimeFunctions.isoTime,
 });
